43be4b504697546edf8f49221cddd8dfe12a8da2
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-01 06:31:07 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Background from '../components/Background';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { GoogleTagManager } from '@next/third-parties/google';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 
 function App({ Component, pageProps }) {
@@ -18,12 +18,12 @@ function App({ Component, pageProps }) {
         strategy="afterInteractive"
       /> */}
 
-      <Script id="CookieDeclaration"
+      {/* <Script id="CookieDeclaration"
         src="https://consent.cookiebot.com/3b999804-26b5-40e7-93bc-f82624a559cc/cd.js"
         type="text/javascript"
         strategy="afterInteractive"
 
-      />
+      /> */}
       <React.StrictMode>
         <ThemeProvider theme={theme}>
           <Normalize />
@@ -31,7 +31,7 @@ function App({ Component, pageProps }) {
           <Background />
           <Header />
           <Component {...pageProps} />
-          {/* <GoogleTagManager gtmId="GTM-P52JLLB7" /> */}
+          <GoogleTagManager gtmId="GTM-P52JLLB7" />
           <Footer />
         </ThemeProvider>
       </React.StrictMode>
