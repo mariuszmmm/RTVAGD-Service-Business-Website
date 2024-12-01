f8d613cbc132dbeca949359b4fa4569e9dcfccfb
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-01 06:21:03 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,23 @@ import Background from '../components/Background';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 
 function App({ Component, pageProps }) {
   return (
     <>
+      {/* <Script
+        src="https://www.googletagmanager.com/gtm.js?id=GTM-P52JLLB7"
+        strategy="afterInteractive"
+      /> */}
+
+      <Script id="CookieDeclaration"
+        src="https://consent.cookiebot.com/3b999804-26b5-40e7-93bc-f82624a559cc/cd.js"
+        type="text/javascript"
+        strategy="afterInteractive"
+
+      />
       <React.StrictMode>
         <ThemeProvider theme={theme}>
           <Normalize />
