b56a8d4b7d10d93d9ff50aa3fb1964a7e0c94095
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-01 04:20:26 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,28 @@
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
-// import { Normalize } from 'styled-normalize';
-// import GlobalStyle from '../styles/GlobalStyle';
-// import React from 'react';
-// import Background from '../components/Background';
-// import Header from '../components/Header';
-// import Footer from '../components/Footer';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from '../styles/GlobalStyle';
+import React from 'react';
+import Background from '../components/Background';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 // import { GoogleTagManager } from '@next/third-parties/google';
 
 
 function App({ Component, pageProps }) {
   return (
     <>
-      {/* <React.StrictMode> */}
-      <ThemeProvider theme={theme}>
-        {/* <Normalize />
+      <React.StrictMode>
+        <ThemeProvider theme={theme}>
+          <Normalize />
           <GlobalStyle />
           <Background />
-          <Header /> */}
-        <Component {...pageProps} />
-        {/* <GoogleTagManager gtmId="GTM-P52JLLB7" /> */}
-        {/* <Footer /> */}
-      </ThemeProvider>
-      {/* </React.StrictMode> */}
+          <Header />
+          <Component {...pageProps} />
+          {/* <GoogleTagManager gtmId="GTM-P52JLLB7" /> */}
+          <Footer />
+        </ThemeProvider>
+      </React.StrictMode>
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-// import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheet } from 'styled-components';
 import Script from 'next/script';
 
 export default class MyDocument extends Document {
-  //   static async getInitialProps(ctx) {
-  //     const sheet = new ServerStyleSheet();
-  //     const originalRenderPage = ctx.renderPage;
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-  //     try {
-  //       ctx.renderPage = () =>
-  //         originalRenderPage({
-  //           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-  //         });
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
 
-  //       const initialProps = await Document.getInitialProps(ctx);
-  //       return {
-  //         ...initialProps,
-  //         styles: (
-  //           <>
-  //             {initialProps.styles}
-  //             {sheet.getStyleElement()}
-  //           </>
-  //         ),
-  //       };
-  //     } finally {
-  //       sheet.seal();
-  //     }
-  //   }
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 
 
 
@@ -64,13 +64,13 @@ export default class MyDocument extends Document {
 
           <Main />
           <NextScript />
-          {/* <Script
+          <Script
             id="google-maps"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = \`https://maps.\${c}apis.com/maps/api/js?\` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({ key: "AIzaSyBdnXnOIJ1jabs-RVE4M2IbwJQ04Ym3xKM", v: "weekly" });`,
             }}
-          /> */}
+          />
         </body>
       </Html>
     );
