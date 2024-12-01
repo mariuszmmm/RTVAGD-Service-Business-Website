6050423cb1a1c3527facdf8d0ce9116c8f1ffb48
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-01 06:47:47 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Background from '../components/Background';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { GoogleTagManager } from '@next/third-parties/google';
-// import Script from 'next/script';
+import Script from 'next/script';
 
 
 function App({ Component, pageProps }) {
@@ -24,6 +24,10 @@ function App({ Component, pageProps }) {
         strategy="afterInteractive"
 
       /> */}
+      <Script
+        src={`https://cdn-cookieyes.com/client_data/342ecae5a1aa8ee84189bffe/script.js`}
+
+      />
       <React.StrictMode>
         <ThemeProvider theme={theme}>
           <Normalize />
@@ -31,7 +35,7 @@ function App({ Component, pageProps }) {
           <Background />
           <Header />
           <Component {...pageProps} />
-          <GoogleTagManager gtmId="GTM-P52JLLB7" />
+          {/* <GoogleTagManager gtmId="GTM-P52JLLB7" /> */}
           <Footer />
         </ThemeProvider>
       </React.StrictMode>
