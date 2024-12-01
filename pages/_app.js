2507ccc372dcb42facfec8a5a8cbb2731fd7baa3
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-01 06:07:49 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function App({ Component, pageProps }) {
           <Background />
           <Header />
           <Component {...pageProps} />
-          <GoogleTagManager gtmId="GTM-P52JLLB7" />
+          {/* <GoogleTagManager gtmId="GTM-P52JLLB7" /> */}
           <Footer />
         </ThemeProvider>
       </React.StrictMode>
