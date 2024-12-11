953f071ce81cf9e3c8da0dfa8d531096eda5046e
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-11 07:19:15 +0100

    test

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='pl'>
         <Head>
-          {/* <link rel="preconnect" href="https://www.googletagmanager.com" />
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
           <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -48,18 +48,18 @@ export default class MyDocument extends Document {
 
           <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
 
-          <meta property="fb:app_id" content="100063811592941" /> */}
+          <meta property="fb:app_id" content="100063811592941" />
         </Head>
         <body>
           <Main />
           <NextScript />
-          {/* <Script
+          <Script
             id="google-maps"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = \`https://maps.\${c}apis.com/maps/api/js?\` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({ key: "AIzaSyBdnXnOIJ1jabs-RVE4M2IbwJQ04Ym3xKM", v: "weekly" });`,
             }}
-          /> */}
+          />
         </body>
       </Html>
     );
