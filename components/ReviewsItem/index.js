ab6362e454725eb76b0d71abf8c8bb8bfeefaee6
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-12 04:32:31 +0100

    test

--- components/ReviewsItem/index.js
+++ components/ReviewsItem/index.js
@@ -9,30 +9,23 @@ import {
 } from "./styled";
 import { Text } from "../common/Text";
 
-const ReviewsItem = ({ item }) => {
-  // if (typeof item !== 'object') {
-  //   console.error('Expected an object but got:', typeof item);
-  //   return null;
-  // }
-
-  return (
-    <ItemWrapper>
-      <Header>
-        <Avatar
-          itemProp="image"
-          src={item.profile_photo_url}
-          alt="Foto"
-          loading="lazy"
-        />
-        <Data >
-          <Author>{item.author_name}</Author>
-          <Time time={item.time} />
-        </Data>
-      </Header>
-      <Stars rating={item.rating || 0} />
-      <Text itemProp="reviewBody" $forReviews>{item.text}</Text>
-    </ItemWrapper>
-  );
-};
+const ReviewsItem = ({ item }) => (
+  <ItemWrapper>
+    <Header>
+      <Avatar
+        itemProp="image"
+        src={item?.profile_photo_url}
+        alt="Foto"
+        loading="lazy"
+      />
+      <Data >
+        <Author>{item?.author_name}</Author>
+        <Time time={item?.time} />
+      </Data>
+    </Header>
+    <Stars rating={item?.rating || 0} />
+    <Text itemProp="reviewBody" $forReviews>{item?.text}</Text>
+  </ItemWrapper>
+);
 
 export default ReviewsItem;
