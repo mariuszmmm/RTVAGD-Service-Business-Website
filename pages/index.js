1716e72fbaffa5df5a0e53fc0292806abebd5ba0
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-16 10:41:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Home from './home';
 import { getReviewsProps } from '../utils/getReviewsProps';
 
-export const getStaticProps = getReviewsProps;
+export const getStaticProps = getRatingProps;
 
 export default Home;
