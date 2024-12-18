0225eaaf11116ec2f1ca6d119922b161aee2db11
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-18 09:31:53 +0100

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -44,7 +44,7 @@ const Header = ({ rating }) => {
         <Logo href="/">
           <Emoticon
             src={imageUrls.logo}
-            alt="emoticon"
+            alt="Logo serwisu RTV i AGD"
             loading="lazy"
             $logo
           />
