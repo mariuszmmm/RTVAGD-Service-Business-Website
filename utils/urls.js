b7cb2be6793e2176dc7004574910f3ea448c158e
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-19 18:24:58 +0100

    Update urls.js

--- utils/urls.js
+++ utils/urls.js
@@ -1,8 +1,10 @@
 export const imageUrls = {
   serwis: "https://res.cloudinary.com/difc0i71u/image/upload/f_auto,q_auto/v1/Serwis/serwis",
   telewizor: "https://res.cloudinary.com/difc0i71u/image/upload/f_auto,q_auto/v1/Serwis/serwis-telewizorow",
-  ekspres: "https://res.cloudinary.com/difc0i71u/image/upload/v1731168333/Serwis/serwis-ekspresow-przemysl-naprawa-ekspresu-przemysl.webp",
-  ekspres_2: "https://res.cloudinary.com/difc0i71u/image/upload/v1733124426/Serwis/serwis-ekspresow-przemysl-profesjonalna-naprawa-ekspresu-przemysl.png",
+  ekspres: "https://res.cloudinary.com/difc0i71u/image/upload/v1731168333/Serwis/serwis-ekspresow-przemysl-naprawa-ekspresu.webp",
+  ekspres_auto: "https://res.cloudinary.com/difc0i71u/image/upload/f_auto,q_auto/v1/Serwis/serwis-ekspresow-przemysl-naprawa-ekspresu",
+  ekspres_2: "https://res.cloudinary.com/difc0i71u/image/upload/v1733124426/Serwis/serwis-ekspresow-przemysl-profesjonalna-naprawa-ekspresu.png",
+  ekspres_2_auto: "https://res.cloudinary.com/difc0i71u/image/upload/f_auto,q_auto/v1/Serwis/serwis-ekspresow-przemysl-profesjonalna-naprawa-ekspresu",
   zmywarka: "https://res.cloudinary.com/difc0i71u/image/upload/f_auto,q_auto/v1/Serwis/serwis-zmywarek",
   pralka: "https://res.cloudinary.com/difc0i71u/image/upload/v1731168333/Serwis/naprawa-pralki.webp",
   suszarka: "https://res.cloudinary.com/difc0i71u/image/upload/f_auto,q_auto/v1/Serwis/naprawa-suszarki",
