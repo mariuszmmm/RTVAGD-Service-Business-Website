c49529bc4935c1add84eb20514831bd905329a24
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-20 11:47:39 +0100

    Update index.js

--- components/common/ImageWrapper/index.js
+++ components/common/ImageWrapper/index.js
@@ -4,14 +4,14 @@ export const ImageWrapper = styled.div`
 position: absolute;
   top: 400px;
   left: 50%;
-  transform: translateX(-170%);
+  transform: translateX(-190%);
   transition: opacity 0.5s ease-in-out;
   opacity: ${({ $show }) => ($show ? 1 : 0)};
-  width: 25vw;
+  width: 20vw;
   height: 25vw;
 
   ${({ $left }) => css`
-    ${$left ? "transform: translateX(-170%)" : "transform: translateX(70%)"};
+    ${$left ? "transform: translateX(-190%)" : "transform: translateX(70%)"};
   `};
 
     ${({ $show }) => !$show && css`
@@ -20,7 +20,7 @@ position: absolute;
     `};
 
 @media(orientation: portrait) {
-  width: 50vw;
+  width: 45vw;
   height: 50vw;
   top: clamp(400px, 82vw, 480px);
   transform: translateX(-50%);
