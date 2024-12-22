542c172e0614292c962687123f3d6ad487637e70
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-22 04:39:09 +0100

    test

--- pages/_document.js
+++ pages/_document.js
@@ -32,15 +32,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang='pl'>
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-X6C7E1S25H"></script>
           <script
             dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-X6C7E1S25H');
-              `,
+              __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P52JLLB7');`,
             }}
           />
           <link rel="preconnect" href="https://www.googletagmanager.com" />
@@ -62,6 +56,8 @@ export default class MyDocument extends Document {
           <meta property="fb:app_id" content="100063811592941" />
         </Head>
         <body>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P52JLLB7"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
           <Script
