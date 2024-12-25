5b0174ac7ce228b139455f13911dc82fd8cd5003
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-25 12:26:36 +0100

    Update index.js

--- components/Background/index.js
+++ components/Background/index.js
@@ -5,14 +5,14 @@ import Image from "next/image.js";
 const Background = () => {
   return (
     <BackgroundWrapper>
-      <ImageContainer>
-        <Image
-          src={imageUrls.serwis}
-          alt="Background image"
-          loading="lazy"
-          fill
-        />
-      </ImageContainer>
+   //   <ImageContainer>
+   //     <Image
+   //       src={imageUrls.serwis}
+   //       alt="Background image"
+   //       loading="lazy"
+   //       fill
+   //     />
+   //   </ImageContainer>
       <Rotating>
         <Circle $top={"0"} $left={"0"}></Circle>
         <Circle $top={"-80%"} $left={"30%"}></Circle>
