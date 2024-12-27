d498b8da8509a2d5518083fe638b78c89d06cb38
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-12-27 15:35:44 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,33 @@ import Background from '../components/Background';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps, rating, reviews }) {
   return (
     <React.StrictMode>
       <ThemeProvider theme={theme}>
         <Normalize />
         <GlobalStyle />
         <Background />
-        <Header {...pageProps} />
-        <Component {...pageProps} />
+        <Header {...pageProps} rating={rating}/>
+        <Component {...pageProps} reviews={reviews}/>
         <Footer />
       </ThemeProvider>
     </React.StrictMode>
   );
 }
+
+export const getStaticProps = async () => {
+  const [ratingProps, reviewsProps] = await Promise.all([
+    getRatingProps(),
+    getReviewsProps(),
+  ]);
+
+  return {
+    props: {
+      ...ratingProps.props,
+      ...reviewsProps.props,
+    },
+  };
+};
 
 export default App;
