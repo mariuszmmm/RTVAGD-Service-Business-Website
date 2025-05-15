2307ba5f0145ac48d1ba8d46ce184fa78ed58e67
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-05-15 07:33:57 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ import Background from '../components/Background';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+// test
+
 function App({ Component, pageProps }) {
   return (
     <React.StrictMode>
